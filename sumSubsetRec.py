fc182aa307ab9b16b8fa5fb6ace822e4758a5551
Riya Gupta <riyag2803@gmail.com>
2020-09-12 22:38:53 +0530

    Update sumSubsetRec.py

--- sumSubsetRec.py
+++ sumSubsetRec.py
@@ -1,2 +1,26 @@
 # Subset Sum Problem
 # Recursive solution
+
+def checkSubset(set,n,sum):
+    #base cases
+    if sum==0:
+        return True
+    if (n==0 and sum!=0):
+        return False
+    
+    #if last element is greater than sum
+    if set[n-1]>sum:
+        return checkSubset(set,n-1,sum)
+    #else check if last element can be included in the sum or not
+    else:
+        return (checkSubset(set,n-1,sum-set[n-1]) or checkSubset(set,n-1,sum))
+
+#driver code
+n=int(input("Enter number of elements: "))
+set = [int(input("Enter element: ")) for i in range(n)]
+sum=int(input("Enter sum: "))
+print(set)
+if checkSubset(set,n,sum)==True:
+    print ("Yes, found.")
+else:
+    print ("Sorry, not found.")
